2e2316e0070aa41dff33bbe898b058b84be0bfe6
Kenny <kenneth@hyperluminalgames.com>
2016-06-02 16:51:00 +0100

    surrounded %~dp0 with quotes incase path has spaces

--- RegisterEngineVersion.cmd
+++ RegisterEngineVersion.cmd
@@ -16,6 +16,6 @@ echo Installing prerequisites...
 start /wait Engine\Extras\Redist\en-us\UE4PrereqSetup_x64.exe /quiet
 
 rem Register the engine installation...
-reg add "HKEY_CURRENT_USER\SOFTWARE\Epic Games\Unreal Engine\Builds" /f /v "%1" /t REG_SZ /d %~dp0
+reg add "HKEY_CURRENT_USER\SOFTWARE\Epic Games\Unreal Engine\Builds" /f /v "%1" /t REG_SZ /d "%~dp0"
 
 )
